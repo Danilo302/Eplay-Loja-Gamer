97ed5f008ee7b12cb714fc01feeaf93edf6ee918
Danilo <danilofelix827@gmail.com>
2025-07-01 22:16:19 -0300

    criando o header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import { Container, GlobalCss } from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <Container>
+        <Header></Header>
+      </Container>
+    </>
+  )
+}
+
+export default App
