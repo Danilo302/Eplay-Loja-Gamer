b2da2e84c9074b8fde081b16c4df7306c6afa2cb
Danilo <danilofelix827@gmail.com>
2025-07-01 23:37:05 -0300

    criando o banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import Banner from './components/Banner'
 import Header from './components/Header'
-import { Container, GlobalCss } from './styles'
+import { GlobalCss } from './styles'
 
 function App() {
   return (
     <>
       <GlobalCss />
-      <Container>
-        <Header></Header>
-      </Container>
+      <div className="container">
+        <Header />
+      </div>
+      <Banner />
     </>
   )
 }
