d3c91a2e575f4ded668918a66ea42bb34e9f32c1
Danilo <danilofelix827@gmail.com>
2025-07-03 11:23:15 -0300

    criando buttons e tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
+const rotas = createBrowserRouter([{ path: '/', element: <Banner /> }])
 
 function App() {
   return (
@@ -9,7 +12,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
