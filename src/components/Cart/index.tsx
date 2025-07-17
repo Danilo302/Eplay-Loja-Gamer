a548aa6cf88b41951e49525fda0f42389e004190
Danilo <danilofelix827@gmail.com>
2025-07-17 13:30:17 -0300

    funcionalidades de adicionar e remover item do carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -10,10 +10,11 @@ import {
   Sidebar
 } from './styles'
 import { RootReducer } from '../../store'
-import { close } from '../../store/reducers/cart'
+import { close, remove } from '../../store/reducers/cart'
+import { formataPreco } from '../ProductsList'
 
 const Cart = () => {
-  const { isOpen } = useSelector((state: RootReducer) => state.cart)
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
 
   const dispatch = useDispatch()
 
@@ -21,25 +22,38 @@ const Cart = () => {
     dispatch(close())
   }
 
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
   return (
     <CartContainer className={isOpen ? 'is-open' : ''}>
       <Overlay onClick={closeCart}></Overlay>
       <Sidebar>
         <ul>
-          <CartItem>
-            <img src="" alt="" />
-            <div>
-              <h3>nome do jogo</h3>
-              <Tag>RPG</Tag>
-              <Tag>PS5</Tag>
-              <span>R$ 150</span>
-            </div>
-            <button type="button" />
-          </CartItem>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreco(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button" />
+            </CartItem>
+          ))}
         </ul>
-        <Quantily> 2 jogos no carrinho</Quantily>
+        <Quantily> {items.length} jogos no carrinho</Quantily>
         <Prices>
-          total de R$ 250.00 <span>Em até 6x sem juros</span>
+          total de {formataPreco(getTotalPrice())}{' '}
+          <span>Em até 6x sem juros</span>
         </Prices>
         <Button title="Clique aqui para continuar a compra" type="button">
           Continuar com a compra
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import { HeaderBar, CartButton, LinkItem, Links } from './styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { open } from '../../store/reducers/cart'
+import { RootReducer } from '../../store'
 
 const Header = () => {
   const dispatch = useDispatch()
+  const { items } = useSelector((state: RootReducer) => state.cart)
 
   const openCart = () => {
     dispatch(open())
@@ -35,7 +37,7 @@ const Header = () => {
         </nav>
       </div>
       <CartButton onClick={openCart}>
-        0 - produto(s)
+        {items.length} - produto(s)
         <img src={carrinho} alt="Carrinho" />
       </CartButton>
     </HeaderBar>
